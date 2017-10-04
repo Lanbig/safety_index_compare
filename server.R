5979e77b21e3b4a403a1c784ec275e227650f70a
Lanbig <lanbig@msn.com>
2017-10-04 15:17:56 -0500

    Finished Cor check page

--- server.R
+++ server.R
@@ -2,40 +2,78 @@ library(shiny)
 library(plotly)
 library(DT)
 library(broom)
-
-data <- read.csv("combined2015.csv")
+library(psych)
 
 shinyServer(function(input, output) {
+  
+  ####### Reactive ##########
+  data <- reactive({
+    year <- input$year
+    inputfile <- paste('combined',year,'.csv', sep = "")
+    print(inputfile)
+    data <- read.csv(inputfile, na.strings = "NULL")
+  })
+  
+  safetydata <- reactive( {
+    columnX <- input$IndicatorX
+    columnY <- input$IndicatorY
+    countryName <- 'country_slug'
+    
+    safetydata <- data()[, c(countryName, columnY, columnX)]
+    safetydata <- safetydata[complete.cases(safetydata), ]
+  })
+  
+  safetydata_m <- reactive({
+      safetydata_m <- lm(safetydata()[-1])
+    })
+  
+  safetydata_cook <- reactive({
+    cooks = cooks.distance(safetydata_m())
+  })
+  
+  safetydata_cor <- reactive({
+    
+    columnX <- input$IndicatorX
+    columnY <- input$IndicatorY
+    cor <- cor.test(safetydata()[,columnX], safetydata()[,columnY], method = "spearman")
+    
+  })
+  
+  ####### Output ##########
+  
+  output$summary_lm <- renderPrint({ 
+    summary(safetydata_m()) 
+  }) 
+  
+  output$summary_cor <- renderPrint({ 
+    print(safetydata_cor())
+  }) 
+  
+  output$view <- DT::renderDataTable({ data() })
+  
    
   output$distPlot <- renderPlotly({
     
     columnX <- input$IndicatorX
     columnY <- input$IndicatorY
-    countryName <- 'country_slug_map'
+    countryName <- 'country_slug'
     
-    safetydata <- data [, c(countryName, columnX, columnY)]
-    safetydata <- safetydata[complete.cases(safetydata), ]
-    
-    m <- lm(safetydata[,columnY] ~ safetydata[,columnX])
-    cooks = cooks.distance(m)
-    
-    
-    plot_ly(x = safetydata[,columnX],y = safetydata[,columnY], name="") %>%  
+    plot_ly(x = safetydata()[,columnX],y = safetydata()[,columnY], text = safetydata()[,countryName]) %>%  
       
-      add_text(text = safetydata[,countryName], color= I("black"),
+      add_text(text = safetydata()[,countryName], color= I("black"),
                textposition = "top right", name ="country Name", visible = "legendonly")  %>%
       
-      add_markers(showlegend = FALSE, 
+      add_markers(showlegend = FALSE, name = "",
                   marker = list(size = 10,
                                 color = 'rgba(0, 0, 0, .0)',
-                                line = list(color = ifelse(cooks > quantile(cooks,.90),'rgba(152, 0, 0, .5)','rgba(0, 0, 0, .5)'),
+                                line = list(color = ifelse(safetydata_cook() > quantile(safetydata_cook(),.90),'rgba(152, 0, 0, .5)','rgba(0, 0, 0, .5)'),
                                             width = 2)))  %>%
       
-      add_lines(y = ~fitted(lm(safetydata[,columnY] ~ safetydata[,columnX])),
+      add_lines(y = ~fitted(lm(safetydata()[,columnY] ~ safetydata()[,columnX])),
                 line = list(color = 'rgba(7, 16 181, 1)'),
                 name = "Linear Line") %>%
       
-      add_ribbons(data = augment(m),
+      add_ribbons(data = augment(safetydata_m()),
                   ymin = ~.fitted - 1.96 * .se.fit,
                   ymax = ~.fitted + 1.96 * .se.fit,
                   line = list(color = 'rgba(7, 164, 181, 0.05)'),
@@ -49,72 +87,70 @@ shinyServer(function(input, output) {
   })
   
   output$residualPlot <- renderPlotly({
-    
+    countryName <- 'country_slug'
     columnX <- input$IndicatorX
     columnY <- input$IndicatorY
-    countryName <- 'country_slug_map'
     
-    safetydata <- data [, c(countryName, columnX, columnY)]
-    safetydata <- safetydata[complete.cases(safetydata), ]
-    
-    m <- lm(safetydata[,columnY] ~ safetydata[,columnX])
-    cooks = cooks.distance(m)
-    
-    
-    plot_ly(x = m$fitted.values,y = m$residuals, color = I("black"), type = 'scatter', 
-            mode = 'text',  text = safetydata[,countryName],
+    plot_ly(x =  safetydata_m()$fitted.values,y =  safetydata_m()$residuals, color = I("black"), type = 'scatter', 
+            mode = 'text',  text = safetydata()[,countryName],
             textposition = "top right", name ="country Name") %>%  
       
       add_markers(showlegend = FALSE, 
                   marker = list(size = 10,
                                 color = 'rgba(0, 0, 0, .0)',
-                                line = list(color = ifelse(cooks > quantile(cooks,.90),'rgba(152, 0, 0, .5)','rgba(0, 0, 0, .5)'),
+                                line = list(color = ifelse(safetydata_cook() > quantile(safetydata_cook(),.90),'rgba(152, 0, 0, .5)','rgba(0, 0, 0, .5)'),
                                             width = 2)))  %>%
       
-      layout(xaxis = list(title = columnX),
-             yaxis = list(title = columnY),
+      layout(xaxis = list(title = 'Fitted Value - Linear Line'),
+             yaxis = list(title = 'Residuals'),
              legend = list(x = 0.80, y = 0.90)) 
     
   })
   
   output$dispData <- DT::renderDataTable({
     
-    columnX <- input$IndicatorX
-    columnY <- input$IndicatorY
+    safetydata <- safetydata()
     
-    #columnX <- 'safety_outcomes'
-    #columnY <- 'ul_safety_index'
-    countryName <- 'country_slug_map'
-    
-    safetydata <- data [, c(countryName, columnX, columnY)]
-    
-     
-    
-    safetydata <- safetydata[complete.cases(safetydata), ]
-    
-    m <- lm(safetydata[,columnY] ~ safetydata[,columnX])
-    cooks = cooks.distance(m)
-    
-    safetydata$Residule <- m$residuals
-    safetydata$Cooks <- cooks
+    safetydata$Residules <- safetydata_m()$residuals
+    safetydata$Cooks <- safetydata_cook()
     
     safetydata
+
   })
  
+  output$dispDataTitle <- renderText({
+    HTML(paste0("<b>Displaying : ",input$year," Data </b>"))
+  })
+
+  output$dispCor <- renderText({
+    HTML(paste0("<br />
+                  <p>Selected Year : ",input$year,
+                "
+                  </p><p>Selected Indicators : </p>
+                  <ul>
+                  <li>",input$IndicatorX,"</li>
+                  <li>",input$IndicatorY,"</li>
+                  </ul>", 
+                
+                "<b>Correlation Coefficient:</b> ",
+                round(safetydata_cor()$estimate, digits = 4),                 
+                "<br/> <b>P-Value:</b> ",
+                round(safetydata_cor()$p.value, digits = 3), 
+                "<br/> <b>Tests of Significance:</b> ",
+                      
+                  if(round(safetydata_cor()$p.value, digits = 4) <= 0.05)
+                    "Passed"
+                  else
+                    "Failed"
+                ))
+  })
   
-  output$summary <- renderPrint({
-    
-    columnX <- input$IndicatorX
-    columnY <- input$IndicatorY
-    
-    #columnX <- 'safety_outcomes'
-    #columnY <- 'ul_safety_index'
-    countryName <- 'country_slug_map'
-    
-    safetydata <- data [, c(countryName, columnX, columnY)]
-    
-    m <- lm(safetydata[,columnY] ~ safetydata[,columnX])
-    summary(m)
-  }) 
-  
+  output$dispDL <- renderText({
+    HTML(paste0('<a href="combined2000.csv" class="btn btn-primary btn-sm">2000</a>',
+                ' <a href="combined2005.csv" class="btn btn-primary btn-sm">2005</a>',
+                ' <a href="combined2010.csv" class="btn btn-primary btn-sm">2010</a>',
+                ' <a href="combined2015.csv" class="btn btn-primary btn-sm">2015</a>'
+                
+    ))
+  })
 })
--- ui.R
+++ ui.R
@@ -7,33 +7,72 @@ safetydata <- read.csv("combined2015.csv")
 
 shinyUI(
   
-  navbarPage("UL Safety Index",
+  navbarPage("UL Safety Index", 
+      
+      theme = shinytheme("lumen"),
              
       tabPanel("Correlation",
       
-               theme = shinytheme("lumen"),
-               
-               sidebarPanel(
-                 selectInput('IndicatorX', 'X: Indicator', c( names(safetydata)), selected = 'transport_injuries'),
-                 selectInput('IndicatorY', 'Y: Indicator', c( names(safetydata)), selected = 'ul_safety_index')
-               ),
-               
-               mainPanel(
-                 
-                 tabsetPanel(
-                   tabPanel( "Scatter Plot", fluidRow(plotlyOutput('distPlot', height = 500), DT::dataTableOutput('dispData') )),
-                   tabPanel( "Residule Plot", plotlyOutput('residualPlot', height = 500), p("... information here ...")  ),
-                   tabPanel( "Summary", verbatimTextOutput("summary"))
-                 )   
-                 
+               fluidPage(
+                 fluidRow(
+                   column(3,
+                          sidebarPanel( width=12, tags$b("Choose: Year and Indicators"),
+                                        selectInput('year', 'Year', c('2000', '2005', '2010', '2015'), selected = '2015'),
+                                        selectInput('IndicatorX', 'X: Indicator', c( names(safetydata)), selected = 'transport_injuries'),
+                                        selectInput('IndicatorY', 'Y: Indicator', c( names(safetydata)), selected = 'ul_safety_index')
+                          ),
+                          
+                          sidebarPanel( width=12, 
+                                        tags$b("Result: Spearman's rank correlation rho "),
+                                        tags$br(),
+                                        htmlOutput("dispCor")
+                                       
+                          ),
+                          
+                          sidebarPanel( width=12, 
+                                        tags$b("Download Dataset .CSV"),
+                                        tags$br(),
+                                        htmlOutput("dispDL")
+                                        
+                          )
+                          
+                   ),
+                   
+                   column(8,
+                          tabsetPanel(
+                            tabPanel( "Scatter Plot", fluidRow(plotlyOutput('distPlot', height = 500), DT::dataTableOutput('dispData') )),
+                            tabPanel( "Residule Plot", plotlyOutput('residualPlot', height = 500), p("")  ),
+                            tabPanel( "Summary", 
+                                      tags$br(),
+                                      tags$b("Spearman's rank correlation rho"),
+                                      verbatimTextOutput("summary_cor"), 
+                                      tags$b("Linear Regression"),
+                                      verbatimTextOutput("summary_lm")),
+                            tabPanel( "Dataset", 
+                                      tags$br(),
+                                      htmlOutput("dispDataTitle"),
+                                      tags$br(),
+                                      dataTableOutput("view"))
+                          ) 
+                   )
+                 )
                )
+               
+               
+
+               
+               
+           
                       
       ),
       
-      tabPanel("Help",
-               p("Nothing but blank page")
-      )
+      tabPanel("Time Series",
+               p("Soon... Nothing but blank page...")
+      ),
 
+      tabPanel("Help",
+               p("Nothing but blank page....")
+      )
           
 
   )
